fb444b217cd54fc915e3dc317a1cb7b6a2bc209f
Abdul Kyume <abbirtak@gmail.com>
2022-05-25 14:08:09 +0600

    faq slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,3 +34,27 @@ function changes2(x){
     console.log(x.childNodes[1]);
     console.log(x.childNodes[3]);
 }
+
+//faq slideshow start
+
+
+var currentIndex = 1;
+displaySlides(currentIndex);
+
+function setSlides(num) {
+    displaySlides(currentIndex += num);
+}
+
+function displaySlides(num) {
+    var x;
+    var slides = document.getElementsByClassName("studentsaycard");
+    if (num > slides.length) { currentIndex = 1 }
+    if (num < 1) { currentIndex = slides.length }
+    for (x = 0; x < slides.length; x++) {
+        slides[x].style.display = "none";
+    }
+    slides[currentIndex - 1].style.display = "block";
+}
+
+
+//faq slidshow end
