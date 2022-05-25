56dfd9ee9d0818f917be7e7fa32ec318618d9b8c
Abdul Kyume <abbirtak@gmail.com>
2022-05-25 22:08:40 +0600

    little changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,15 @@ window.onload = function()
  
 window.onscroll = function () { 
     if (document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 100) {
-        myNav.classList.remove("d-none");
         myNav.classList.add("nav-colored");
+        myNav.classList.add("responsivenav");
         myNav.classList.remove("nav-transparent");
         
     } 
     else {
         myNav.classList.add("nav-transparent");
-        myNav.classList.add("d-none");
         myNav.classList.remove("nav-colored");
+        myNav.classList.remove("responsivenav");
     }
 };
 function changes(x){
